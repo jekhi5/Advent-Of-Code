f2a1d26dde6a30737501b6eae2c178e13983d662
jekhi5 <>
2022-12-08 18:06:32 -0500

    Finished day 8!

--- 2022/Day 8 - Treetop Tree House/Solution.py
+++ 2022/Day 8 - Treetop Tree House/Solution.py
@@ -21,23 +21,37 @@ def take_pass(inpt, result, index_of_left):
 
     for row in range(len(inpt)):
         cur_max = 0
+        index_of_max = 0
         for col in range(len(inpt[row])):
             
             cur_height = inpt[row][col]
 
             # We're on the edge and that tree is visible
             if col == 0: 
-                result[row][col][index_of_left] = True
+                result[row][col][index_of_left] = (True, 0, (row, col), inpt[row][col])
                 cur_max = inpt[row][col]
+                index_of_max = 0
+            elif cur_height > cur_max:
+                result[row][col][index_of_left] = (True, col, (row, col), inpt[row][col])
+                cur_max = inpt[row][col]
+                index_of_max = col
             else:
-                if cur_height > cur_max:
-                    result[row][col][index_of_left] = True
-                    cur_max = inpt[row][col]
+
+                # Determine how many trees can be seen from this location if it cannot be seen from the edge
+                
+                count = 0
+                for i in range(col - 1, index_of_max - 1, -1):
+                    count += 1
+                    if inpt[row][i] >= cur_height:
+                        break
+
+                result[row][col][index_of_left] = (False, count, (row, col), inpt[row][col])
+                if cur_height == cur_max:
+                    index_of_max = col
+     
+
 
 def rotate90Clockwise(A):
-    #for row in A:
-        #print(row)
-
     N = len(A[0])
     for i in range(N // 2):
         for j in range(i, N - i - 1):
@@ -50,30 +64,30 @@ def rotate90Clockwise(A):
 
 def Solution(inpt):
     # Every elem in result is a list of 4 representing if it's visible from left, bottom, right, up
-    result = [ [ [None] * 4 for i in range(len(inpt[0]))] for j in range(len(inpt))]
+    result = [ [ [False] * 4 for i in range(len(inpt[0]))] for j in range(len(inpt))]
     
     for i in range(4):
         take_pass(inpt, result, i)
-        #print("ROTATING INPUT")
         rotate90Clockwise(inpt)
-        
-        #for row in inpt:
-            #print(row)
-        
-        #print("ROTATING RESULT")
         rotate90Clockwise(result)
-        #for row in result:
-           # print(row)
     
-    count = 0
+    count_of_visible = 0
+    best_scenic_score = -1
     for row in result:
         for col in row:
-            if col[0] or col[1] or col[2] or col[3]:
-                count += 1
-    #print("!!!!!!!")
-    rotate90Clockwise(result)
-    return count
+            if col[0][0] or col[1][0] or col[2][0] or col[3][0]:
+                count_of_visible += 1
+            
+            cur_score = col[0][1] * col[1][1] * col[2][1] * col[3][1]
 
-print(Solution(loi))
+            best_scenic_score = max(best_scenic_score, cur_score)
+
+    
+    return (count_of_visible, best_scenic_score)
+
+result = Solution(loi)
+
+print(result[0])
+print(result[1])
 
 
