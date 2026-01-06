040ee55be64ebd61e8452c29d8006b9228774ef7
Jacob Kline <jacobk513@gmail.com>
2026-01-05 23:11:46 -0500

    optimize 2025 day 7

--- 2025/7.py
+++ 2025/7.py
@@ -1,7 +1,8 @@
 manifold = []
+memoized_manifold = []
 
 
-def find_ceiling_splitter(row, col):
+def find_ceiling_splitter(row: int, col: int) -> int:
     for cur_row in range(row - 1, 0, -1):
         if manifold[cur_row][col] != ".":
             return cur_row
@@ -9,7 +10,7 @@ def find_ceiling_splitter(row, col):
     return 0
 
 
-def sum_above_timelines(row, col):
+def sum_above_timelines(row: int, col: int) -> int:
     ceiling_splitter_row = find_ceiling_splitter(row, col)
 
     # This is the first splitter and there is only one particle that can pass through it
@@ -18,20 +19,23 @@ def sum_above_timelines(row, col):
 
     total = 0
     for cur_row in range(row - 1, ceiling_splitter_row, -1):
-        left = manifold[cur_row][col - 1] if col > 0 else None
-        right = manifold[cur_row][col + 1] if col < len(manifold[cur_row]) - 1 else None
-        total += left if left != None and left != "." and left != "S" else 0
-        total += right if right != None and right != "." and right != "S" else 0
+        left = memoized_manifold[cur_row][col - 1] if col > 0 else None
+        right = (
+            memoized_manifold[cur_row][col + 1]
+            if col < len(memoized_manifold[cur_row]) - 1
+            else None
+        )
+        total += left if left != None else 0
+        total += right if right != None else 0
 
     return total
 
 
-def find_total_timelines():
-    for row in range(len(manifold)):
-        for col in range(len(manifold[row])):
-            spot = manifold[row][col]
+def find_total_timelines() -> int:
+    for i, row in enumerate(manifold):
+        for j, spot in enumerate(row):
             if spot == "^":
-                manifold[row][col] = sum_above_timelines(row, col)
+                memoized_manifold[i][j] = sum_above_timelines(i, j)
 
     return sum(
         [
@@ -42,8 +46,8 @@ def find_total_timelines():
 
 
 def find_total_splits(cur_row, beam_col):
-    if cur_row == len(manifold) - 1:
-        # End of manifold
+    if cur_row == len(manifold) - 1 or manifold[cur_row + 1][beam_col] == "|":
+        # End of manifold or overlapping beam
         return 0
     if manifold[cur_row + 1][beam_col] == ".":
         # No splitter, move straight down
@@ -52,20 +56,23 @@ def find_total_splits(cur_row, beam_col):
     if manifold[cur_row + 1][beam_col] == "^":
         # Split
         if beam_col == 0:
-            # Don't overlap with existing beams
+            # On left edge
+            # Don't overlap with existing beams to the RIGHT
             if manifold[cur_row + 1][beam_col + 1] == "|":
                 return 1
 
             manifold[cur_row + 1][beam_col + 1] = "|"
             return find_total_splits(cur_row + 1, beam_col + 1) + 1
         elif beam_col == len(manifold[cur_row]) - 1:
-            # Don't overlap with existing beams
+            # On right edge
+            # Don't overlap with existing beams to the LEFT
             if manifold[cur_row + 1][beam_col - 1] == "|":
                 return 1
 
             manifold[cur_row + 1][beam_col - 1] = "|"
             return find_total_splits(cur_row + 1, beam_col - 1) + 1
         else:
+            # Somewhere in the middle of the manifold
             if (
                 manifold[cur_row + 1][beam_col - 1] == "|"
                 and manifold[cur_row + 1][beam_col + 1] == "|"
@@ -89,8 +96,6 @@ def find_total_splits(cur_row, beam_col):
                     + find_total_splits(cur_row + 1, beam_col + 1)
                     + 1
                 )
-    if manifold[cur_row + 1][beam_col] == "|":
-        return 0
     raise ValueError(f"Illegal manifold state: {manifold}")
 
 
@@ -101,4 +106,8 @@ with open("2025/inputs/7.txt") as inpt:
     starting_beam_col = "".join(manifold[0]).find("S")
     print("Solution for part 1: ", find_total_splits(0, starting_beam_col))
     manifold = [list(line) for line in lines]
+    memoized_manifold = [
+        [0 if char == "^" else None for char in row] for row in manifold
+    ]
+
     print("Solution for part 2: ", find_total_timelines())
